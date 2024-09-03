6d7d365046d04842dd8f097c92ad995f39f46656
meganarmas <meganarmas@gmail.com>
2024-09-03 08:09:45 -0500

    fix navigation

--- marvel—template/src/componets/Navigation.jsx
+++ marvel—template/src/componets/Navigation.jsx
@@ -3,21 +3,21 @@ import { NavLink } from "react-router-dom";
 const Navigation = () => {
     return (
         <nav className="nav-bar">
-            <NavLink to="/" exact activeClassName="active-link" className="nav-link">
+            <NavLink to="/" exact activeClassName="active">
                 Home 
             </NavLink>
-            <NavLink to="/browse-characters" exact activeClassName="active-link" className="nav-link">
+            <NavLink to="/browse-characters" exact activeClassName="active">
                 Browse Characters
             </NavLink>
-            <NavLink to="/detail-characters" exact activeClassName="active-link" className="nav-link">
+            <NavLink to="/detail-characters" exact activeClassName="active">
                 Detail Characters
             </NavLink>
-            <NavLink to="/comics" exact activeClassName="active-link" className="nav-link">
+            <NavLink to="/comics" exact activeClassName="active">
                 Comics
             </NavLink>
           
         </nav>
-    )
+    );
 }
 
 export default Navigation;
